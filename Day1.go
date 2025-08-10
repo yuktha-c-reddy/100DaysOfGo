09bba59aab5fda292352b921e1d516ff8e6d6331
Yuktha <70469290+yuktha-c-reddy@users.noreply.github.com>
2025-08-10 12:38:25 +0530

    Update Day1.go
    Build a simple CLI that inputs user details for savings plan

--- Day1.go
+++ Day1.go
@@ -1,17 +1,45 @@
 // Day 1–2: Setup Go, variables, constants, fmt.
 
-// Mini Project: Personal Finance Greeting CLI — takes name, monthly income, prints a welcome message with saving goals.
+// Mini Project: Personal Finance Greeting CLI — takes name, monthly income, calculates saving goals.
 
 package main
-import "fmt" //formatted i/p & o/p
+import "fmt"
 
-func main(){
-    fmt.Println("Personal Finance Greeting CLI")
-    fmt.Println("Enter name and monthly income")
-    var income float32
-    var name string
-    fmt.Scanf("%s %f",&name,&income)
-    fmt.Printf("Dear %s , Kindly save at least %.2f rupees per month\n",name, 0.2*income)   
-}
-
+func main() {
+  fmt.Println("SIP Calculator")
+  var name string
+  var income float32
+  var dur int
+  var investment float32
+  var minSavings float32
+  fmt.Println("Enter your name and monthly income")
+  fmt.Scanf("%s %f",&name , &income)
+  if (name=="" || income<=0){
+      fmt.Println("Please enter correct name and positive income")
+  } else{
+      minSavings=0.2*income
+      var choice string
+      fmt.Println("You have to save atleast 20% of your income = ",minSavings)
+      fmt.Println("Do you want to change the savings amount? Y/N")
+      
+      fmt.Scanf("%s",&choice)
+      if choice=="Y"{
+        fmt.Println("Enter monthly investment and no.of months")
+        fmt.Scanf("%d %f",&dur,&investment)
+        if (dur<=0 || investment<0){
+            fmt.Println("Please enter positive values")
+        }else{
+            total_invested:=(float32(dur)*investment)
+            fmt.Printf("Total investment is %.2f \n" ,total_invested)
+        }
+      }else if choice=="N"{
+         fmt.Println("Calculating savings per annum for ",minSavings)
+         fmt.Println(minSavings*12.0)
+      } else {
+          fmt.Println("Please enter valid choice")
+      }
+      
+    }
+  
+  }
 
